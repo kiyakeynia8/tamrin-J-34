9d2adf8b52ec8bd721a8cefe7f251a40ddbb0278
kiyakeynia8 <118113533+kiyakeynia8@users.noreply.github.com>
2023-06-21 11:57:33 +0330

    Add files via upload

--- tamrin/1/main1.py
+++ tamrin/1/main1.py
@@ -1,19 +1,9 @@
 import cv2
-import numpy as np
+from kiya_RGB_to_gray import RGB_to_gray
+
 
 image = cv2.imread("input/rgb.jpg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-def RGB_to_gray(image):
-    s, c, _ = image.shape
-    result = np.zeros((s,c),dtype= np.uint8)
-    r, g, b = cv2.split(image)
-
-    for i in range(s):
-        for j in range(c):
-            result[i,j] = (r[i,j] + g[i,j] + b[i,j]) // 3
-
-    return result
 
 image_gray = RGB_to_gray(image)
 
